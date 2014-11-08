c9cf71f4d1a8135ef2ad3bfca3d019fcaf189c76
damian88raymond <damian88.raymond@gmail.com>
2014-11-08 18:14:12 -0500

    INdex1

---
+++ maps/index_1.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jean-Mathieu
+ * Date: 6/11/14
+ * Time: 6:17 AM
+ */
+
+?>
+
+<!doctype html>
+<html>
+<head>
+
+    <meta charset="utf-8"/>
+    <title>Maps Test</title>
+    <style>
+      html, body, #map {
+        height: 100%;
+        margin: 0px;
+        padding: 0px;
+      }
+      #combo {
+        position: fixed;
+        top: 5px;
+        left: 50%;
+        margin-left: -55px;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+      }
+      </style>
+    <script src="https://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        // Initialize the Google Maps API v3
+        var domain = "generateXML.php";
+        var markers = [];
+        var map;
+        var x = 0;
+        var infoWindow;
+        var marker;
+        var markersArray = [];
+        var customIcons = {
+            0: {
+                icon: 'images/all.png'
+            },
+            1: {
+                icon: 'images/school.png'
+            },
+            2: {
+                icon: 'images/spirituality.png'
+            },
+            3: {
+                icon: 'images/recreation.png'
+            },
+            4: {
+                icon: 'images/legal.png'
+            },
+            5: {
+                icon: 'images/health.png'
+            },
+            6: {
+                icon: 'images/family.png'
+            },
+            7: {
+                icon: 'images/sex.png'
+            }
+        };
+
+            function initialize() {
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: new google.maps.LatLng(43.6558658, -79.380568),
+                    zoom: 10,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+                navigator.geolocation.getCurrentPosition(function(position) {
+        
+            var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                infoWindow = new google.maps.InfoWindow({
+                map: map,
+                position: geolocate,
+                content:"<h1>here</h1>"
+            });
+            
+
+                update();
+            
+            map.setCenter(geolocate);
+            
+        });
+                }
+
+                function clearOverlays() {
+                    for (var i = 0; i < markersArray.length; i++) {
+                        markersArray[i].setMap(null);
+                    }
+                }
+
+
+
+            function update(){
+                clearOverlays();
+                downloadUrl( domain, function (data) {
+                    var xml = data.responseXML;
+                    var markers = xml.documentElement.getElementsByTagName("marker");
+                    console.log(markers);
+                    for (var i = 0; i < markers.length; i++) {
+                        var title = markers[i].getAttribute("title");
+                        var desc = markers[i].getAttribute("desc");
+                        var location = markers[i].getAttribute("location");
+                        var category = markers[i].getAttribute("category");
+                        var type = markers[i].getAttribute("alert");
+                        var point = new google.maps.LatLng(
+                            parseFloat(markers[i].getAttribute("lat")),
+                            parseFloat(markers[i].getAttribute("lng")));
+                        var html = "<b>" + title + "<br>" + location + "<br>" + category + + "</b>";
+                        var icon = customIcons[type] || {};
+                        marker = new google.maps.Marker({
+                            map: map,
+                            position: point,
+                            icon: icon.icon
+                        });
+                        markersArray.push(marker);
+
+                        bindInfoWindow(marker, map, infoWindow, html);
+                    }
+                });
+
+            }
+
+        function bindInfoWindow(marker, map, infoWindow, html) {
+            google.maps.event.addListener(marker, 'click', function () {
+                infoWindow.setContent(html);
+                infoWindow.open(map, marker);
+            });
+        }
+
+        function downloadUrl(url, callback) {
+            var request = window.ActiveXObject ?
+                new ActiveXObject('Microsoft.XMLHTTP') :
+                new XMLHttpRequest;
+
+            request.onreadystatechange = function () {
+                if (request.readyState == 4) {
+                    request.onreadystatechange = doNothing;
+                    callback(request, request.status);
+                }
+            };
+
+            request.open('GET', url, true);
+            request.send(null);
+        }
+        function doNothing() {
+        }
+
+        function getCategory(){
+            var e = document.getElementById('category').value;
+            domain = "generateXML.php";
+            domain = domain + "?category=" + e;
+            //alert(domain);
+            update();
+        }
+
+    </script>
+</head>
+<body onload="initialize();">
+    <div id="map"></div>
+    <div id="combo">
+        <select id="category" onchange="getCategory();">
+            <option value=""></option>
+            <option value="Legal%20and%20Financial">Legal & Financial</option>
+            <option value="Health%20and%20Social">Health & Social</option>
+            <option value="Recreation%20and%20Culture">Recreation & Culture</option>
+            <option value="Spirituality%20and%20Wellbeing">Spirituality & Wellbeing</option>
+            <option value="Work%20and%20School">Work & School</option> 
+            <option value="Sex%20and%20Relationships">Sex & Relationships</option>
+        </select>
+    </div>
+     </body>
+</html>
