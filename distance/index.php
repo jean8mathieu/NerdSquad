e9f72598f900d32b31dd36a66a774799ce43effb
jackie588727 <huangquanzhi@hotmail.com>
2014-11-08 16:34:23 -0500

    finally able to sync

--- distance/index.php
+++ distance/index.php
@@ -2,18 +2,46 @@
 
 $lati = $_GET['lati'];
 $long = $_GET['long'];
+$mode = $_GET['mode'];
+$key = "AIzaSyC1KB-Idnu0DIKC7o0EWIxf7RMJbnblgVQ";
 
-$json = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=43.65807,-79.378394&destinations=".$lati.",".$long."&mode=bicycling&language=en-EN&key=AIzaSyC1KB-Idnu0DIKC7o0EWIxf7RMJbnblgVQ";
+function walking($cur_lati,$cur_long,$lati,$long,$key){
+    return "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=walking&language=en-EN&key=".$key;
+}
 
-$ch = curl_init($json);
+function driving($cur_lati,$cur_long,$lati,$long,$key){
+    return "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=drving&language=en-EN&key=".$key;
+}
+
+function bicycling ($cur_lati,$cur_long,$lati,$long,$key){
+    return "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=bicycling&language=en-EN&key=".$key;
+}
+
+function getDistance($result){
+    return $result['rows'][0]['elements'][0]['distance']['text'];
+}
+
+function getDuration($result){
+    return $result['rows'][0]['elements'][0]['duration']['text'];
+}
+
+if($mode == "w")
+    $url = walking($cur_lati,$cur_long,$lati,$long,$key);
+else if($mode == 'd')
+    $url = driving($cur_lati,$cur_long,$lati,$long,$key);
+else if($mode == 'b')
+    $url = bicycling($cur_lati,$cur_long,$lati,$long,$key);
+else
+    print "ERROR";
+    
+
+$ch = curl_init($url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-$result = json_decode($result, true);
+$result = json_decode($result, true);   //decode json
 
-$final = $result['rows'][0]['elements'][0]['distance']['text'];
-
-print_r ($final);
-
+print_r (getDistance($result));
+print_r(getDuration($result));
 ?>
