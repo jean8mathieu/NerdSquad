5e08ec4fce617f3b6398acd012f8ccae967e5f4f
Sunny Pathak <sunny.pathak@sheridancollege.ca>
2014-11-08 15:13:30 -0500

    illkillu

--- maps/index.php
+++ maps/index.php
@@ -142,7 +142,7 @@
     ?>-->
 
     <div id="map" style="height: 100%; margin: 0px; padding: 0px;"/>
-    <div id=“combo” style=“position: absolute; top: 5px; left: 50%; margin-left: -180px; z-index: 5; background-color: #fff; padding: 5px; border: 1px solid #999;” />
+    <div id="combo" style="position: absolute; top: 5px; left: 50%; margin-left: -180px; z-index: 5; background-color: #fff; padding: 5px; border: 1px solid #999;" />
     <!--<article class="module width_3_quarter">
     </article>-->
     <div class="spacer"></div>
