dd40e8bf3e42abf1fac1b15d620bece941524d93
jackie588727 <huangquanzhi@hotmail.com>
2014-11-08 20:57:10 -0500

    updated resource

--- resource/index.php
+++ resource/index.php
@@ -1,11 +1,22 @@
+<script src="https://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+
+
 <?php
 
+if (isset($_GET["cla"]) && isset($_GET["clo"])) {
+    $cur_lati = $_GET["cla"];
+    $cur_long = $_GET["clo"];
+}else{
+
+}
+
+getbyCate($cur_lati,$cur_long,"Family",5);
 
 
-getbyCate("Family");
+function getbyCate($cur_lati,$cur_long,$cate,$kmfilter){
 
 
-function getbyCate($cur_lati,$cur_long,$cat,$kmfilter){
+    include("../includes/destinationInfo.php");
 
     $SQLhost = "thoughtspot.db.9124079.hostedresource.com";
     $SQLusername = "thoughtspot";
@@ -16,29 +27,52 @@ function getbyCate($cur_lati,$cur_long,$cat,$kmfilter){
 
     mysql_select_db($SQLdatabase);
 
+    $result = mysql_query("SELECT location,description,category,latitude,longitude,address1,city,postalCode,phone,website FROM Locations WHERE category LIKE  '%".$cate."%'");
 
-    $result = mysql_query("SELECT location,description,category,latitude,longitude,address1,city,postalCode,phone,website FROM Locations WHERE category LIKE  '%".$cat."%'");
+    $count = 0;
 
-    while($row = mysql_fetch_array($result,MYSQL_ASSOC)){
-        if(distanceFilter($cur_lati,$cur_long, $row["latitude"],$row["longitude"],$kmfilter))
-        printf("Category: %s Location: %s  Latitude: %s Longitude: %s "."<br />\r\n", $row["category"],$row["location"], $row["latitude"], $row["longitude"] );
+    while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+        $distance = distanceFilter($cur_lati, $cur_long, $row["latitude"], $row["longitude"]);
+        if($count <= 10) {
+            if ($distance <= $kmfilter) {
+                printf("Category: %s  Latitude: %s Longitude: %s Distance from you: %s" . "<br />\r\n", $row["category"], $row["latitude"], $row["longitude"], $distance);
+                $count ++;
+            }
+        }else{
+            break;
+        }
     }
 
 }
 
-function distanceFilter($cur_lati,$cur_long,$lati,$long,$request_dis){
-    include("..includes/destinationInfo.php");
+function distanceFilter($cur_lati,$cur_long,$lati,$long){
 
+    include("../includes/destinationInfo.php");
 
 
     $distance = walkingDis($cur_lati,$cur_long,$lati,$long);
 
-    $newphrase = chop(" km", $distance);
+    $newphrase = substr($distance,0,-3);
 
-    if($newphrase <= $request_dis){  //within distance of request
-        return true;
-    }else{
-        return false;
-    }
+    return $newphrase;
 
 }
+
+?>
+
+<script type="text/javascript">
+
+    $(document).ready(){
+
+    navigator.geolocation.getCurrentPosition(locationHandler);
+
+        function locationHandler(position){
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+
+            window.location.href = "index.php?cla=" + lat + "&clo=" + lng;
+
+        }
+    }
+
+</script>
