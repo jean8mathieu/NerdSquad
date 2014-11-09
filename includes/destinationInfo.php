ac42540c15a7bc881f8c98ac7308ed8789832094
jackie588727 <huangquanzhi@hotmail.com>
2014-11-08 19:06:32 -0500

    added radius filter for resource

--- includes/destinationInfo.php
+++ includes/destinationInfo.php
@@ -4,40 +4,40 @@ $key = "AIzaSyC1KB-Idnu0DIKC7o0EWIxf7RMJbnblgVQ";
 
 
 
-function walkingTime($cur_lati,$cur_long,$lati,$long,$key){
+function walkingDis($cur_lati,$cur_long,$lati,$long,$key){
     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=walking&language=en-EN&key=".$key;
     $result = decodeJson($url);
-    print_r (getDistance($result));
+    return (getDistance($result));
 }
 
 function walkingDur($cur_lati,$cur_long,$lati,$long,$key){
     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=walking&language=en-EN&key=".$key;
     $result = decodeJson($url);
-    print_r (getDur($result));
+    return (getDur($result));
 }
 
-function drivingTime($cur_lati,$cur_long,$lati,$long,$key){
+function drivingDis($cur_lati,$cur_long,$lati,$long,$key){
     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=driving&language=en-EN&key=".$key;
     $result = decodeJson($url);
-    print_r (getDistance($result));
+    return (getDistance($result));
 }
 
 function drivingDur($cur_lati,$cur_long,$lati,$long,$key){
     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=driving&language=en-EN&key=".$key;
     $result = decodeJson($url);
-    print_r (getDur($result));
+    return (getDur($result));
 }
 
-function bicyclingTime($cur_lati,$cur_long,$lati,$long,$key){
+function bicyclingDis($cur_lati,$cur_long,$lati,$long,$key){
     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=bicycling&language=en-EN&key=".$key;
     $result = decodeJson($url);
-    print_r (getDistance($result));
+    return (getDistance($result));
 }
 
 function bicyclingDur($cur_lati,$cur_long,$lati,$long,$key){
     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$cur_lati.",".$cur_long."&destinations=".$lati.",".$long."&mode=bicycling&language=en-EN&key=".$key;
     $result = decodeJson($url);
-    print_r (getDur($result));
+    return (getDur($result));
 }
 
 function getDistance($result){
--- resource/index.php
+++ resource/index.php
@@ -5,7 +5,7 @@
 getbyCate("Family");
 
 
-function getbyCate($cat){
+function getbyCate($cur_lati,$cur_long,$cat,$kmfilter){
 
     $SQLhost = "thoughtspot.db.9124079.hostedresource.com";
     $SQLusername = "thoughtspot";
@@ -20,9 +20,25 @@ function getbyCate($cat){
     $result = mysql_query("SELECT location,description,category,latitude,longitude,address1,city,postalCode,phone,website FROM Locations WHERE category LIKE  '%".$cat."%'");
 
     while($row = mysql_fetch_array($result,MYSQL_ASSOC)){
+        if(distanceFilter($cur_lati,$cur_long, $row["latitude"],$row["longitude"],$kmfilter))
         printf("Category: %s Location: %s  Latitude: %s Longitude: %s "."<br />\r\n", $row["category"],$row["location"], $row["latitude"], $row["longitude"] );
     }
 
+}
 
+function distanceFilter($cur_lati,$cur_long,$lati,$long,$request_dis){
+    include("..includes/destinationInfo.php");
+
+
+
+    $distance = walkingDis($cur_lati,$cur_long,$lati,$long);
+
+    $newphrase = chop(" km", $distance);
+
+    if($newphrase <= $request_dis){  //within distance of request
+        return true;
+    }else{
+        return false;
+    }
 
 }
